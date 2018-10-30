5e9ac12c460ed5d01b3dba601487e25e7fe450f0
DEBARSHI2018 <44364988+DEBARSHI2018@users.noreply.github.com>
2018-10-31 02:44:12 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,7 +13,7 @@ makeCacheMatrix<- function(x= matrix()){
   setinv<-function(inv) i<<-inv
   getinv<- function() i
   list(set= set, get= get,setinv= setinv,getinv=getinv)
-}  ## this function cache the matrix provided by the reader
+}  ## this function sets an environment and cache the matrix provided by the reader
 cacheSolve<- function(x, ...){
   i<- x$getinv()
   if(!is.null(i)){
@@ -24,7 +24,8 @@ cacheSolve<- function(x, ...){
   i<-solve(data, ...)
   x$setinv(i)
   i
-} ## this function calculates the inversee of the matrix provided by the reader and puts the result in the designated space in previous function
+} ## this function calculates the inverse of the matrix provided by the reader and puts the result in the designated space in previous function
 
 
 ## Write a short comment describing this function
+
